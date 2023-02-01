595356b1e7696ffb49360cca4af327583e42df84
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-01 20:47:58 +0200

    11

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.feedback-form');
 
+console.log(form);
+form.email.addEventListener('input', formlInputEmail);
+form.message.addEventListener('input', formlInputMessage);
+form.addEventListener('submit', formSubmit);
+
+function formlInputEmail(event) {
+  const emailValue = event.target.value;
+
+  console.log('Пишу в мило', emailValue);
+}
+
+function formlInputMessage(event) {
+  const messageValue = event.target.value;
+
+  console.log('Пишу в масагу', messageValue);
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+
+  console.log('removeItem');
+}
