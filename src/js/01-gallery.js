7b05c401590744369e52908d1d5899f2e4437f6e
Igor Boichuk <ihor.boichuk@gmail.com>
2023-02-02 11:25:50 +0200

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const myGallery = document.querySelector('.gallery');
+
+const greateMyGallery = () => {
+  for (const item of galleryItems) {
+    let imgArray = [];
+    const createItem = `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>`;
+    imgArray.push(createItem);
+    const joinArray = imgArray.join('');
+    myGallery.insertAdjacentHTML('afterbegin', joinArray);
+  }
+};
+greateMyGallery();
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
