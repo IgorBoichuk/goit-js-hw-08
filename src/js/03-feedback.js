210a9d4b1e5045c7c667e7ee28941aaca8f4e5ce
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-02 23:04:02 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,32 @@
+const { throttle } = require('lodash');
+
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', formlInputEmail);
+form.addEventListener('input', throttle(formDataInput, 500));
 form.addEventListener('submit', formSubmit);
 
-function formlInputEmail(event) {
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+const KEY_STORAGE = 'feedback-form-state';
 
-  const inputData = `{${email.name}: ${email.value}, ${message.name}: ${message.value}}`;
-  localStorage.setItem('feedback-form-state', inputData);
+const inputData = {};
+
+const savedSettings = localStorage.getItem(KEY_STORAGE);
+const parsedSettings = JSON.parse(savedSettings);
+console.log(parsedSettings.email);
+
+// if (parsedSettings.email) {
+//   form.email.value = parsedSettings.email;
+// }
+// if (parsedSettings.message) {
+//   form.message.value = parsedSettings.message;
+// }
+
+function formDataInput(event) {
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(inputData));
 }
+
+//  f20@online.ua
+// dobry den everybody
 
 function formSubmit(event) {
   event.preventDefault();
