92d1303704913dd395ef20b9ed23d676cfc0b580
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-02 19:22:57 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,17 +7,16 @@ const player = new Player(iframe);
 const setCurrentTime = () => {
   if (localStorage.getItem('videoplayer-current-time')) {
     player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+    player.on(
+      'timeupdate',
+      throttle(event => {
+        localStorage.setItem('videoplayer-current-time', event.seconds);
+      }, 1000)
+    );
   }
   return;
 };
 setCurrentTime();
-
-player.on(
-  'timeupdate',
-  throttle(event => {
-    localStorage.setItem('videoplayer-current-time', event.seconds);
-  }, 1000)
-);
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
