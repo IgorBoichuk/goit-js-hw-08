a68f7f56a78ace504377453f9c6e555e0cf21b6c
IgorBoichuk <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-02 18:50:06 +0000

    Deploying to gh-pages from @ IgorBoichuk/goit-js-hw-08@d926e5adfeb8c9423585c92bcb277a67702e8faa 🚀

--- 03-feedback.14bd86df.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){e.target.value,e.target.value;console.log(e.target.value)})),e.addEventListener("submit",(function(e){e.preventDefault(),localStorage.removeItem("feedback-form-state"),e.currentTarget.reset(),console.log("removeItem")}));
-//# sourceMappingURL=03-feedback.14bd86df.js.map
--- 03-feedback.f02c4668.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){e.target.value,e.target.value;console.log(e.target.value)})),e.addEventListener("submit",(function(e){e.preventDefault(),localStorage.removeItem("feedback-form-state"),e.currentTarget.reset(),console.log("removeItem")}))}();
-//# sourceMappingURL=03-feedback.f02c4668.js.map
