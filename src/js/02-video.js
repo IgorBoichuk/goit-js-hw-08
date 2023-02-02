cbeece38b8b40444a3457712d613ed8333982a2a
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-02 19:20:36 +0200

    8/2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,44 +1,24 @@
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
-console.dir(Player);
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+const setCurrentTime = () => {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+  return;
+};
+setCurrentTime();
 
 player.on(
   'timeupdate',
   throttle(event => {
     localStorage.setItem('videoplayer-current-time', event.seconds);
   }, 1000)
-  // console.log('played the video!');
-  //   localStorage.setItem('videoplayer-current-time', event.seconds);
 );
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
-
-// player.on(
-//   'timeupdate',
-//   throttle(e => {
-//     localStorage.setItem('videoplayer-current-time', e.seconds);
-//   }, 1000)
-// );
-
-// ------------------підключити пізніше
-// player
-//   .setCurrentTime(30.456)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
